64ab301a98260f626a1a02db592d382e56559243
Ujjwal Bagve <ujjwal.bagwe@here.com>
2020-03-15 20:31:22 +0530

    Refactored : Used Replace Temp with Query for rest of variables

--- src/main/java/com/refactoring/tutorial/movierenter/model/Customer.java
+++ src/main/java/com/refactoring/tutorial/movierenter/model/Customer.java
@@ -20,29 +20,42 @@ public class Customer {
     }
 
     public String getStatement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
+
+
         Enumeration rentals = this.rentals.elements();
         StringBuffer sb = new StringBuffer();
         sb.append("Rental Record For " + getName() + "\n");
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
-
             sb.append("\t").append(each.getMovie().getMovieName()).append("\t");
             sb.append(each.getCharge() + "\n");
-            totalAmount += each.getCharge();
         }
         //Generate footer line for Message
-        sb.append("Amount owned is ").append(totalAmount).append("\n");
-        sb.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
+        sb.append("Amount owned is ").append(getTotalCharge()).append("\n");
+        sb.append("You earned ").append(getTotalFrequentRenterPoints()).append(" frequent renter points");
         return sb.toString();
     }
 
 
+    private Double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = this.rentals.elements();
 
-    private double getAmount(Rental rental){
-        return rental.getCharge();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
     }
 
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = this.rentals.elements();
+
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
 }
