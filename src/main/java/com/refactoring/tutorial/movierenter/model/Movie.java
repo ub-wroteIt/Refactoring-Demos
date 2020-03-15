82814d0b11f8552abac530cd807064bb8b2e2599
Ujjwal Bagve <ujjwal.bagwe@here.com>
2020-03-15 20:55:31 +0530

    Refactored : Use Move method to delegate responsibility on correct class
    Type is more voltile info, So if chnage type we try to minimize the ripple Effect

--- src/main/java/com/refactoring/tutorial/movierenter/model/Movie.java
+++ src/main/java/com/refactoring/tutorial/movierenter/model/Movie.java
@@ -14,4 +14,33 @@ public class Movie {
 
     private String movieName;
     private int priceCode;
+
+    double getCharge(int noOfDaysRented) {
+       double result = 0;
+       switch (getPriceCode()) {
+           case REGULAR:
+               result += 2;
+               if (noOfDaysRented > 2) {
+                   result += (noOfDaysRented - 2) * 1.5;
+               }
+               break;
+           case NEW_RELEASE:
+               result += noOfDaysRented * 3;
+               break;
+           case CHILDREN:
+               result += 1.5;
+               if (noOfDaysRented > 3)
+                   result += (noOfDaysRented - 3) * 1.5;
+               break;
+       }
+       return result;
+   }
+
+    int getFrequentRenterPoints(int noOfDaysRented) {
+        //add bouns for two days new release rental
+        if (getPriceCode() == NEW_RELEASE && noOfDaysRented > 1) {
+            return 2;
+        }
+        return 1;
+    }
 }
--- src/main/java/com/refactoring/tutorial/movierenter/model/Rental.java
+++ src/main/java/com/refactoring/tutorial/movierenter/model/Rental.java
@@ -12,32 +12,12 @@ public class Rental {
     private Movie movie;
     private int noOfDaysRented;
 
-     double getCharge() {
-        double amount = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (getNoOfDaysRented() > 2) {
-                    amount += (getNoOfDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                amount += getNoOfDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                amount += 1.5;
-                if (getNoOfDaysRented() > 3)
-                    amount += (getNoOfDaysRented() - 3) * 1.5;
-                break;
-        }
-        return amount;
+    int getFrequentRenterPoints() {
+        return this.movie.getFrequentRenterPoints(noOfDaysRented);
     }
 
-     int getFrequentRenterPoints() {
-        //add bouns for two days new release rental
-        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getNoOfDaysRented() > 1) {
-            return 2;
-        }
-        return 1;
+    double getCharge() {
+        return this.movie.getCharge(noOfDaysRented);
     }
+
 }
